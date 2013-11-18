fea9a24efedaf106037d3b0e57af7e54bf879e9f
Zekarias Araya <zekarias99@gmail.com>
2013-11-17 17:04:03 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tewolde::Application.config.secret_key_base = '3c0efcd55906e8d2b7f0527f30a65c7357348f4ce5d82e77beee5c18db4866a83c410cc9ac58e43246d24a754a80757ac446991d1e39e40ade525d4a77709ac7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tewolde::Application.config.session_store :cookie_store, key: '_tewolde_session'
