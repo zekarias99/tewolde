842344f37f0be9b61d1eeafe45348ae5fc96ca43
Zekarias Araya <zekarias99@gmail.com>
2013-11-19 10:39:06 -0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Tewolde')
     end
+
+    it "should have the right title 'Tewolde | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Tewolde | Home')
+    end
   end
 
   describe "Help page" do
@@ -15,6 +20,11 @@ describe "Static pages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+    it "should have the right title 'Tewolde | Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Tewolde | Help')
     end
   end
 
@@ -24,6 +34,11 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+
+    it "should have the right title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Tewolde | About Us')
+    end
   end
 
   describe "Contact us page" do
@@ -31,6 +46,11 @@ describe "Static pages" do
     it "should have the content 'Contact Us'" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact Us')
+    end
+
+    it "should have title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Tewolde | Contact Us')
     end
   end
 end
